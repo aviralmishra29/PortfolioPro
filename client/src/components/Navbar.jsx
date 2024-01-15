e1069ef6ec51a82f01d64fb2d8534efae52e178f
Aviral Mishra <mishraviral1@gmail.com>
2024-01-15 16:27:39 +0530

    Navbar added

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-gray-700 text-white flex flex-row justify-center' >Portfolio Pro</div>
+  )
+}
+
+export default Navbar
